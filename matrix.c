27ac883fd5d0b22e0755513ee07fa74b86fac51e
Raffael Bottoli Schemmer <raffael.schemmer@acad.pucrs.br>
2018-03-01 01:27:11 -0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,133 +1,106 @@
-/**************************************************************************************
-	Task of Master to manage the MPI parallelization
- **************************************************************************************/
-multiReturn matrixMultiMPI(matrix A, matrix B, matrix D){
-  printf("\nStart parallel multiplication with MPI ... \n");
-  double startMPI = MPI_Wtime();
-  
-  row = B.rows;
-  col = B.cols;
-  // Prepare dimensions of B and A -> store in array -> so all could be send in 1 step
-  int *AB_dim_1D = malloc(3*sizeof(int *)); 
-  	AB_dim_1D[0] = row;
-  	AB_dim_1D[1] = col;
-  // copy 2D-Array into 1D-Array for B -> needed in every process
-  int *B_matrix_1D = malloc(row*col*sizeof(int *));
-	  for(i=0;i<row;i++){
-			for(j=0;j<col;j++){
-				B_matrix_1D[i*row+j] = B.matrix[i][j];
-			}
-	  }
-  
-  int slave;
-  int numberOfSlaves = size-1; // Master is no slave
-  int average_rows = A.rows/numberOfSlaves; // e.g. 10/4 = 2
-  int extra_rows = A.rows%numberOfSlaves; // rest = 2 
-  
-  startrow = 0;
-  for(slave=1; slave <= numberOfSlaves; slave++){
-	
-	row = average_rows;
-	if(slave <= extra_rows){row = average_rows+1;}
-	AB_dim_1D[2] = row; // will also be send
-	
-	int *A_matrix_1D = malloc(row*A.cols*sizeof(int *)); // Prepare matrixparts of A for each slave
-		int c=0;
-		for(i=startrow;i<startrow+row;i++){
-				for(j=0;j<A.cols;j++){
-					// copy part of 2D array into 1D array
-					A_matrix_1D[c] = A.matrix[i][j];
-					c++;
-				}
-		}	
-		startrow += row; // remember position for next slave
-		
-	// Send all Data: 1. Dimensions  2. Matrix B  3. special part of A for each slave	
-	MPI_Send(AB_dim_1D, 3, MPI_INT, slave, MTYPE_FROM_MASTER, MPI_COMM_WORLD);
-	MPI_Send(B_matrix_1D, B.rows*B.cols, MPI_INT, slave, MTYPE_FROM_MASTER, MPI_COMM_WORLD);
-	MPI_Send(A_matrix_1D, row*A.cols, MPI_INT, slave, MTYPE_FROM_MASTER, MPI_COMM_WORLD);	
-	
-   	free(A_matrix_1D);
-  }
-  
-  free(AB_dim_1D);
-  free(B_matrix_1D);
-  
-  
-  // Return from slaves
-  startrow = 0;
-  for(slave=1; slave <= numberOfSlaves; slave++){
-  	
-	row = average_rows;
-	if(slave <= extra_rows){row = average_rows+1;}
-	
-	int *D_matrix_1D = malloc(row*B.cols*sizeof(int *));
-	// Receive part of result matrix D from every slave
-	MPI_Recv(D_matrix_1D, row*B.cols, MPI_INT, slave, MTYPE_FROM_SLAVE, MPI_COMM_WORLD, &status);
-	
-		int c=0;
-		for(i=startrow;i<startrow+row;i++){
-				for(j=0;j<A.cols;j++){
-					// copy 1D array to the right pos. in the 2D array D
-					D.matrix[i][j] = D_matrix_1D[c];
-					c++;
-				}
-				
-		}	
-		startrow += row; // remember the position
-		
-		free(D_matrix_1D);
-	
-  }
-  double endMPI = MPI_Wtime();
-  printf("finished\n\n");
-  
-  multiReturn mr = {"MPI",endMPI-startMPI};
-  
-  return mr;
+/* --------------------------------------------------------------------------------------
+
+Obj do Fonte : Algoritimo paralelo de multiplicação matricial em C (Utilizando MPI)
+Disciplina   : CMP 256 2013/II UFRGS PPGC (Prf. Nicolas Bruno Maillard) 
+Data         : 03/12/2013
+Autor        : Raffael Bottoli Schemmer
+
+--------------------------------------------------------------------------------------- */
+
+// Bibliotecas utilizadas
+#include "mpi.h" // Mpi para comunicacao em rede entre os nós
+#include <stdio.h> // Biblioteca padrão para entrada e saida.
+
+#define SIZE 1024			/* Size of matrices */
+// Constante que define o tamanho da matriz.
+// Esse valor pode ser parametrizável seguindo as restrições:
+// #1 - Apenas valores base 2 são aceitos Ex:(2/4/8/16/32/64) não existindo restrição de tamanho.
+// #2 - As matrizes serão sempre quadráticas, no sentido que SIZE define tanto o número de linhas como de colunas.
+
+unsigned long int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+// Declaração das matrizes (Parte estática (SIZE))
+
+// Inicializa a matriz A com valores crescente (A partir de zero)
+void fill_matrixA(unsigned long int m[SIZE][SIZE])
+{
+  static unsigned long int n=0;
+  int i, j;
+  for (i=0; i<SIZE; i++)
+    for (j=0; j<SIZE; j++)
+      m[i][j] = n++;
 }
 
-/**************************************************************************************
-	TASK OF SLAVES
- **************************************************************************************/  
-void beMySlave(){
-
- 	int* AB_dim_1D = malloc(3*sizeof(int *));
- 	// 1. receive the dimensions of the matrices
- 	MPI_Recv(AB_dim_1D, 3, MPI_INT, MASTER, MTYPE_FROM_MASTER, MPI_COMM_WORLD, &status);
- 	
- 	int B_rows = AB_dim_1D[0];
- 	int B_cols = AB_dim_1D[1];
- 	int A_rows = AB_dim_1D[2];
- 	
- 	// allocate memory depending on first recv-step
- 	int *B_matrix_1D = malloc(B_rows*B_cols*sizeof(int *));
- 	// 2. receive the whole B matrix
- 	MPI_Recv(B_matrix_1D, B_rows*B_cols, MPI_INT, MASTER, MTYPE_FROM_MASTER, MPI_COMM_WORLD, &status);
- 	
- 	int *A_matrix_1D = malloc(A_rows*B_rows*sizeof(int *));
- 	// 3. receive a special part of A matrix for calculations
- 	MPI_Recv(A_matrix_1D, A_rows*B_rows, MPI_INT, MASTER, MTYPE_FROM_MASTER, MPI_COMM_WORLD, &status);
- 	
- 	// prepare the D matrix, which contains the result data
- 	int *D_matrix_1D = malloc(A_rows*B_cols*sizeof(int *));
- 	int i,j,k;
- 	for(i=0;i<A_rows;i++){
- 		for(j=0;j<B_cols;j++){
- 			int tempSum = 0;
- 			for(k=0;k<B_rows;k++){
- 				// simulate 2D matrix multiplication with 2 1D matrices
- 				tempSum += A_matrix_1D[i*B_rows+k] * B_matrix_1D[k*B_cols+j];
- 				
- 			}
- 			D_matrix_1D[i*B_cols+j] = tempSum;
- 		}
- 	}
- 	// after finishing work, slaves send back their results to master
- 	MPI_Send(D_matrix_1D, A_rows*B_cols, MPI_INT, MASTER, MTYPE_FROM_SLAVE, MPI_COMM_WORLD);
- 	
-	free(AB_dim_1D);
-	free(A_matrix_1D);
-	free(B_matrix_1D);
-	free(D_matrix_1D);
+// Inicia a matriz B com valores decrescentes (A partir de SIZE*SIZE-1)
+void fill_matrixB(unsigned long int m[SIZE][SIZE])
+{
+  static unsigned long int n=(SIZE*SIZE)-1;
+  int i, j;
+  for (i=0; i<SIZE; i++)
+    for (j=0; j<SIZE; j++)
+      m[i][j] = n--;
 }
+
+// Mostra a matriz C calculada na tela (Usada para depuração)
+void print_matrix(unsigned long int m[SIZE][SIZE])
+{
+  int i, j = 0;
+  for (i=0; i<SIZE; i++) {
+    printf("\n\t| ");
+    for (j=0; j<SIZE; j++)
+      printf("%2lu ", m[i][j]);
+    printf("|");
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int myrank, P, from, to, i, j, k;
+  MPI_Status status;
+  
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);	// Ranking do nó
+  MPI_Comm_size(MPI_COMM_WORLD, &P); // Número total de nós do cluster
+
+  // Somente calcula matrizes divisíveis pelo número de processadores
+  if (SIZE%P!=0) {
+    if (myrank==0) printf("Matrix size not divisible by number of processors\n");
+    MPI_Finalize();
+    exit(-1);
+  }
+
+  // Variáveis utilizadas para separar a matriz A (SIZE) em linhas comparado ao número de threads (P).
+  from = myrank * SIZE/P;
+  to = (myrank+1) * SIZE/P;
+
+  // Inicializa as matrizes A e B
+  if (myrank==0) {
+    fill_matrixA(A);
+    fill_matrixB(B);
+  }
+ 
+  // Realiza o broadcast transmitindo a matriz B para todos os nós (Nó 0 root somente irá realizar o broadcast).
+  MPI_Bcast (B, 2*(SIZE*SIZE), MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  
+  // Realiza a transmissão das partes (linhas) da matriz A entre todos os nós (Nó 0 root somente irá realizar o scatter).
+  MPI_Scatter (A, 2*(SIZE*SIZE/P), MPI_UNSIGNED, A[from], 2*(SIZE*SIZE/P), MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  
+  // Realiza a computação das linhas que cada thread foi designada para processar (Todos os nós executam computação sobre a matriz A)
+  for (i=from; i<to; i++) 
+    for (j=0; j<SIZE; j++) {
+      C[i][j]=0;
+      for (k=0; k<SIZE; k++)
+	C[i][j] += A[i][k]*B[k][j];
+    }
+  
+  // Realiza a recepção das partes (linhas) da matriz C de cada um dos nós (Nó 0 root somente irá realizar o gather).
+  MPI_Gather (C[from], 2*(SIZE*SIZE/P), MPI_UNSIGNED, C, 2*(SIZE*SIZE/P), MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  
+  // DUMP - Escreve a matriz C na tela (Descomente as linhas de código abaixo)
+  /*
+	if(myrank == 0)
+		print_matrix(C);
+  */		
+  MPI_Finalize();
+  return 0;
+}
+
